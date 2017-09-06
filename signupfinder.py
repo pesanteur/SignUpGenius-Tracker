a27969a8f5cf8629d6bdf0e1fb89a1ad6dca5ac3
pesanteur <pesanteur123@gmail.com>
2017-09-06 15:05:28 -0400

    Update

--- signupfinder.py
+++ signupfinder.py
@@ -10,3 +10,10 @@ soup = BeautifulSoup(data, 'lxml')
 
 #main_table = soup.find('td', attrs={'class': 'SUGmain'})
 main_table = soup.find('table', attrs={'class': 'SUGtableouter'})
+
+# main_table doesn't pull day of the week for some reason
+# TODO: arrange table/list data by the day of the week that they are on
+
+test = [i.replace('\n', '').strip() for i in table_data[0]]
+#TODO: add if statement to edit the following sample output
+# ['Date (mm/dd/yyyy)', 'Time (AST)', 'Calendar View\t\t\t\tAvailable Slot']
