4227f99295aed3efd3bd09b7cfd1efeb6e952107
pesanteur <pesanteur123@gmail.com>
2017-09-07 12:24:30 -0400

    Added check for dates to help with poor table design

--- signupfinder.py
+++ signupfinder.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
 url = "http://www.signupgenius.com/index.cfm?go=s.signup&urlid=10c084baaa82da2fe3-term159&view=standard"
 
@@ -14,6 +15,19 @@ main_table = soup.find('table', attrs={'class': 'SUGtableouter'})
 # main_table doesn't pull day of the week for some reason
 # TODO: arrange table/list data by the day of the week that they are on
 
+table_data = [[cell.text for cell in row('td')] for row in main_table('tr')]
+# TODO: maybe simplify this, the table is not as complex as this thinks
+
 test = [i.replace('\n', '').strip() for i in table_data[0]]
 #TODO: add if statement to edit the following sample output
 # ['Date (mm/dd/yyyy)', 'Time (AST)', 'Calendar View\t\t\t\tAvailable Slot']
+
+# TODO: search first place in each list to check for date
+date_regex = re.compile(r'ˆ\d{1,2}\/\d{1,2}\/\d{4}')
+def find_date(data):
+	date_regex = re.compile(r'^\d{1,2}\/d{1,2}\/\d{4}')
+	search_date = date_regex.search(data)
+	if search_date:
+		return True
+
+# TODO: Now use the above function to make a list in a list? 
