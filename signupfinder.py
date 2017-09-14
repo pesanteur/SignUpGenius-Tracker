47c1efb74c21b13e6b545e01bc14d27a182a4c28
pesanteur <pesanteur123@gmail.com>
2017-09-14 12:58:29 -0400

    Added OrderedDict logic, now need to build out

--- signupfinder.py
+++ signupfinder.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+from collections import OrderedDict
 
 url = "http://www.signupgenius.com/go/10c084baaa82da2fe3-term162"
 
@@ -10,6 +11,20 @@ r = requests.get(url)
 data = r.text
 soup = BeautifulSoup(data, 'lxml')
 
+""""The following class allows us to append to an OrderedDict"""
+class MyOrderedDict(OrderedDict):
+	def append(self, key, value):
+		root = self._OrderedDict__root
+		last = root[0]
+
+		if key in self:
+			raise KeyError
+		else:
+			root[0] = last[1] = self._OrderedDict__map[key] = [last, root, key]
+			dict.__setitem__(self, key, value)
+
+
+
 #main_table = soup.find('td', attrs={'class': 'SUGmain'})
 main_table = soup.find('table', attrs={'class': 'SUGtableouter'})
 
@@ -18,6 +33,7 @@ main_table = soup.find('table', attrs={'class': 'SUGtableouter'})
 
 table_data = [[cell.text for cell in row('td')] for row in main_table('tr')]
 # TODO: maybe simplify this, the table is not as complex as this thinks
+# TODO: this code duplicates some information from the table...possibly due to the merge structure of the initial column
 
 test = [i.replace('\n', '').strip() for i in table_data[0]]
 #TODO: add if statement to edit the following sample output
@@ -39,16 +55,32 @@ table_first = [item[0] for item in table_data] # gets the first item in the subl
 def find_date(data):
 	""""Checks whether content of a cell is a date"""
 	date_regex = re.match(r'^\d{1,2}\/\d{1,2}\/\d{4}', data)
-	if data_regex is not  None:
+	if date_regex is not  None:
 		return True
 	else:
 		return False
 
 list_for_dates = [] # Use a better variable name
+sub_list = []
+
 count = 0 # Useful to track number of cells without date, so we know what to add to first occuring date
 
-for i in range(len(table_data)):
+for i in range(len(table_data)): # Maybe just use table_first up top?
 	cell_1 = table_data[i][0]
 	if find_date(cell_1):
 		list_for_dates.append(cell_1)
-	print(i) # For now
+	
+print(list_for_dates) # For now
+
+# Test logic, TODO: use a ordered dictionary instead of a list of lists
+big_list = []
+inner_list = []
+
+
+
+if find_date(table_data[1][0]): # iterate over the first position with a for loop
+	big_list.append(table_data[1][0])
+	inner_list.append(table_data[1][1:])
+	d = OrderedDict([(big_list, inner_list)])
+	# Append to MyOrderedDict using class
+	d.append() # Ordering is messed up here figure this out
